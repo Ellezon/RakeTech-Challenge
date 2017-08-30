c67b68ec2131c94f9d9c3af1b4c7f548bb5172bf
Ellezon <mikkifalz@gmail.com>
2017-08-30 16:47:20 +0200

    Display tables

--- DB/DB_fns.php
+++ DB/DB_fns.php
@@ -4,19 +4,19 @@ function get_tasks()
 {
     $link = connect_db();
     $sql = "SELECT * FROM `tasks`";
+    $tasks = [];
     $result = mysqli_query($link, $sql);
     if ($result->num_rows > 0) 
     {
-        echo "<table  border = \"1\" style=\"width:80%; table-layout: fixed;text-align:center;   margin-left:10%\";>";
-        
         while ($row = mysqli_fetch_object($result)) 
         {
-            
-            $name = $row->task_title;								  
-            echo "<tr><td><h1>$name</h1></td>";
+            $name = $row->task_title;
+            array_push($tasks, $name);
+            //echo "<tr><td><h1>$name</h1></td><td></td><td></td></tr>";
         }
-        echo "</table>";
+        return $tasks;
     }
+    return [];
 }
 
 ?>
--- index.php
+++ index.php
@@ -3,7 +3,6 @@
     include('DB/DB_fns.php');
     include ('HTML/header.html');
     $tasks = get_tasks();
-    define("tasks",$tasks);
     include ('HTML/index.html');
     include ('HTML/footer.html');
 ?>
