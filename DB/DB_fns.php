5ced472e4bae24b442c8c607d55d48684c18a9f8
Ellezon <mikkifalz@gmail.com>
2017-09-01 00:04:14 +0200

    total hours updated on edit

--- DB/DB_fns.php
+++ DB/DB_fns.php
@@ -1,45 +1,52 @@
 <?php
+
 include ('DB_connect.php');
-if (isset($_GET["del_id"])) {
-    $del_id = json_decode($_GET["del_id"], false);
-    $outp = delete_task($del_id);
-    unset($_GET["del_id"]);
+if (isset($_GET["del_id"]))
+{
+				$del_id = json_decode($_GET["del_id"], false);
+				$outp = delete_task($del_id);
+				unset($_GET["del_id"]);
 }
-if (isset($_POST["edit_id"])) {
-    $edit_info = new stdClass();
+if (isset($_POST["edit_id"]))
+{
+				$edit_info = new stdClass();
 
 
-    $edit_id = json_decode($_POST["edit_id"], false);
-    if (isset($_POST["edit_notes"])) {
-        $edit_notes = $_POST["edit_notes"];
-        $edit_info->notes = $edit_notes;
-    }
-    if (isset($_POST["edit_title"])) {
-        $edit_title = $_POST["edit_title"];
-        $edit_info->title = $edit_title;
-    }
-    if (isset($_POST["edit_hours"])) {
-        $edit_hours = $_POST["edit_hours"];
-        $edit_info->hours = $edit_hours;
-    }
+				$edit_id = json_decode($_POST["edit_id"], false);
+				if (isset($_POST["edit_notes"]))
+				{
+								$edit_notes = $_POST["edit_notes"];
+								$edit_info->notes = $edit_notes;
+				}
+				if (isset($_POST["edit_title"]))
+				{
+								$edit_title = $_POST["edit_title"];
+								$edit_info->title = $edit_title;
+				}
+				if (isset($_POST["edit_hours"]))
+				{
+								$edit_hours = $_POST["edit_hours"];
+								$edit_info->hours = $edit_hours;
+				}
 
-    $outp = edit_task($edit_id, $edit_info);
-    unset($_POST["edit_id"]);
-    unset($_POST["edit_notes"]);
-    $edit_info = null;
+				$outp = edit_task($edit_id, $edit_info);
+				unset($_POST["edit_id"]);
+				unset($_POST["edit_notes"]);
+				$edit_info = null;
 }
 
 
 function connect_and_get($status, $keyword, $title)
 {
-    $link = connect_db();
-    $sql = "SELECT `task_title`, `task_hours`, `task_ID`, `task_notes` FROM `tasks` WHERE `task_status` = $status";
-    $result = mysqli_query($link, $sql);
-    if ($result->num_rows > 0) {
-        $totalHours = 0;
-        $rowno = -1;
+				$link = connect_db();
+				$sql = "SELECT `task_title`, `task_hours`, `task_ID`, `task_notes` FROM `tasks` WHERE `task_status` = $status";
+				$result = mysqli_query($link, $sql);
+				if ($result->num_rows > 0)
+				{
+								$totalHours = 0;
+								$rowno = -1;
 
-        echo " 
+								echo " 
                     <div id= '$keyword-div'>
                         <table>
                             <thead>
@@ -49,37 +56,41 @@ function connect_and_get($status, $keyword, $title)
                             </thead>
                             <tbody>";
 
-        while ($row = mysqli_fetch_object($result)) {
-            $name = $row->task_title;
-            $id = $row->task_ID;
-            $hours = $row->task_hours;
-            $totalHours += $hours;
-            $rowno++;
-            $table = "\"$keyword\"";
-            $edit_id = $keyword . "_edit";
-            $edit_id = str_replace("\"", "", $edit_id);
-            $buttonclass = $keyword . "_button";
-            $buttonclass = str_replace("\"", "", $buttonclass);
-            $delete_id = $keyword . "_delete";
-            $delete_id = str_replace("\"", "", $delete_id);
-            $notes_id = $keyword . "_notes_". $rowno;
-            $notes_id = str_replace("\"", "", $notes_id);
-            $title_id = $keyword . "_title_" . $rowno;
-            $title_id = str_replace("\"", "", $title_id);
-            $hours_id = $keyword . "_hours_". $rowno;
-            $hours_id = str_replace("\"", "", $hours_id);            
-            $yes_id = $keyword . "_yes";
-            $yes_id = str_replace("\"", "", $yes_id);
-            $info_rows = $keyword . "_info";
-            $info_rows = str_replace("\"", "", $info_rows);
-            $edit_buttons = $keyword . "_editbuttons";
-            $edit_buttons = str_replace("\"", "", $edit_buttons);
-            $notes = $row->task_notes;
-            if (empty($row->task_notes)) {
-                $notes = "None";
-            }
+								while ($row = mysqli_fetch_object($result))
+								{
+												$name = $row->task_title;
+												$id = $row->task_ID;
+												$hours = $row->task_hours;
+												$totalHours += $hours;
+												$rowno++;
+												$table = "\"$keyword\"";
+												$edit_id = $keyword . "_edit";
+												$edit_id = str_replace("\"", "", $edit_id);
+												$buttonclass = $keyword . "_button";
+												$buttonclass = str_replace("\"", "", $buttonclass);
+												$delete_id = $keyword . "_delete";
+												$delete_id = str_replace("\"", "", $delete_id);
+												$notes_id = $keyword . "_notes_" . $rowno;
+												$notes_id = str_replace("\"", "", $notes_id);
+												$title_id = $keyword . "_title_" . $rowno;
+												$title_id = str_replace("\"", "", $title_id);
+												$hours_id = $keyword . "_hours_" . $rowno;
+												$hours_id = str_replace("\"", "", $hours_id);
+												$yes_id = $keyword . "_yes";
+												$yes_id = str_replace("\"", "", $yes_id);
+												$info_rows = $keyword . "_info";
+												$info_rows = str_replace("\"", "", $info_rows);
+												$edit_buttons = $keyword . "_editbuttons";
+												$edit_buttons = str_replace("\"", "", $edit_buttons);
+                                                $totalhrs = $keyword . "_totalhours";
+                                                $totalhrs = str_replace("\"", "", $totalhrs);
+												$notes = $row->task_notes;
+												if (empty($row->task_notes))
+												{
+																$notes = "None";
+												}
 
-            echo "  <tr>
+												echo "  <tr>
                         <td id= '$title_id' onclick='show_info($table,$id, $rowno);'>$name</td>
                         <td id= '$hours_id'>$hours</td>
                     </tr>
@@ -95,12 +106,12 @@ function connect_and_get($status, $keyword, $title)
                     <td> <button  onclick='cancel_editing($table)' class ='$buttonclass $keyword'>Cancel</button> </td>
                     </tr>
                     ";
-        }
-        echo " </tbody>
+								}
+								echo " </tbody>
                 <tfoot>
                     <tr class = '$keyword'>
                         <td>Total Hours</td>
-                        <td>$totalHours</td>
+                        <td id =$totalhrs>$totalHours</td>
                 </tr>
                 </tfoot>
         </table>       
@@ -111,66 +122,73 @@ function connect_and_get($status, $keyword, $title)
       </div>";
 
 
-    }
+				}
 }
 
 function delete_task($del_id)
 {
-    $link = connect_db();
-    if ($link->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
+				$link = connect_db();
+				if ($link->connect_error)
+				{
+								die("Connection failed: " . $conn->connect_error);
+				}
 
-    $sql = "DELETE FROM `tasks` WHERE `task_ID` = $del_id";
-    if ($link->query($sql) === true) {
-        echo "Record deleted successfully";
-    } else {
-        echo "Error deleting record: " . $link->error;
-    }
+				$sql = "DELETE FROM `tasks` WHERE `task_ID` = $del_id";
+				if ($link->query($sql) === true)
+				{
+								echo "Record deleted successfully";
+				} else
+				{
+								echo "Error deleting record: " . $link->error;
+				}
 }
 
 function edit_task($edit_id, $edit_info)
 {
-    $link = connect_db();
-    if ($link->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    $sqlpt = "";
-    if(isset($edit_info->notes))
-    {
-        $sqlpt = $sqlpt.", `task_notes` = \"$edit_info->notes\"";
-    }
-    if(isset($edit_info->hours))
-    {
-        $sqlpt = $sqlpt.",`task_hours` = $edit_info->hours";
-    }
-    if(isset($edit_info->title))
-    {
-        $sqlpt = $sqlpt.",`task_title` = \"$edit_info->title\"";
-    }
-    
-    //remove starting comma
-    $sqlpt = preg_replace('/^,+|,+$/', '', $sqlpt);
-    $sql = "UPDATE `tasks` SET $sqlpt WHERE `task_ID` = $edit_id";
-    //error_log("all: ".$sql);
-    if ($link->query($sql) === true) {
-        echo "Record updated successfully";
-    } else {
-        echo "Error deleting record: " . $link->error;
-    }
+				$link = connect_db();
+				if ($link->connect_error)
+				{
+								die("Connection failed: " . $conn->connect_error);
+				}
+				$sqlpt = "";
+				if (isset($edit_info->notes))
+				{
+								$sqlpt = $sqlpt . ", `task_notes` = \"$edit_info->notes\"";
+				}
+				if (isset($edit_info->hours))
+				{
+								$sqlpt = $sqlpt . ",`task_hours` = $edit_info->hours";
+				}
+				if (isset($edit_info->title))
+				{
+								$sqlpt = $sqlpt . ",`task_title` = \"$edit_info->title\"";
+				}
+
+				//remove starting comma
+				$sqlpt = preg_replace('/^,+|,+$/', '', $sqlpt);
+				$sql = "UPDATE `tasks` SET $sqlpt WHERE `task_ID` = $edit_id";
+				//error_log("all: ".$sql);
+				if ($link->query($sql) === true)
+				{
+								echo "Record updated successfully";
+				} else
+				{
+								echo "Error deleting record: " . $link->error;
+				}
 
 }
 function get_tasks()
 {
-    $link = connect_db();
-    if ($link->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    echo " <div id= 'tasks'>";
-    connect_and_get("0", "todo", "To-Do");
-    connect_and_get("1", "progress", "In Progress");
-    connect_and_get("2", "done", "Done");
-    echo "</div>";
+				$link = connect_db();
+				if ($link->connect_error)
+				{
+								die("Connection failed: " . $conn->connect_error);
+				}
+				echo " <div id= 'tasks'>";
+				connect_and_get("0", "todo", "To-Do");
+				connect_and_get("1", "progress", "In Progress");
+				connect_and_get("2", "done", "Done");
+				echo "</div>";
 }
 
 ?>
--- JS/details.js
+++ JS/details.js
@@ -2,32 +2,34 @@ var task_id = null;
 var notes_elemid = null;
 var title_elemid = null;
 var hours_elemid = null;
+var totalhours_elemid = null;
 var editing = false;
 var notes_value = null;
 var title_value = null;
 var hours_value = null;
 
+
 function show_info(table, id, rowno)
-{    if(!editing)    
+{
+	if (!editing)
 	{
-	var classname = table + "_info";
-	if (document.getElementsByClassName(classname)[rowno + rowno].style.display == "table-row")
-	{
-		document.getElementsByClassName(classname)[rowno + rowno].style.display = "none";
-		document.getElementsByClassName(classname)[rowno + rowno + 1].style.display = "none";
+		var classname = table + "_info";
+		if (document.getElementsByClassName(classname)[rowno + rowno].style.display == "table-row")
+		{
+			document.getElementsByClassName(classname)[rowno + rowno].style.display = "none";
+			document.getElementsByClassName(classname)[rowno + rowno + 1].style.display = "none";
+		}
+		else
+		{
+			document.getElementsByClassName(classname)[rowno + rowno].style.display = "table-row";
+			document.getElementsByClassName(classname)[rowno + rowno + 1].style.display = "table-row";
+		}
 	}
-	else
-	{
-		document.getElementsByClassName(classname)[rowno + rowno].style.display = "table-row";
-		document.getElementsByClassName(classname)[rowno + rowno + 1].style.display = "table-row";
-	}
-    }
 };
 
 function open_dialogue(id, delete_id, keyword)
 {
-    
-	   	   var idname = "#" + keyword + "_yes";
+	var idname = "#" + keyword + "_yes";
 	$(delete_id).dialog(
 	{
 		minHeight: 30
@@ -47,122 +49,121 @@ function open_dialogue(id, delete_id, keyword)
 			}
 		});
 	})
-      
 };
 
 function cancel_editing(keyword)
 {
-     $(notes_elemid).html("Notes: "+ notes_value);
-    $(title_elemid).html(title_value);
-    $(hours_elemid).html(hours_value);
-    stop_editing(keyword);
+	$(notes_elemid).html("Notes: " + notes_value);
+	$(title_elemid).html(title_value);
+	$(hours_elemid).html(hours_value);
+	stop_editing(keyword);
 }
 
 function stop_editing(keyword)
 {
-    var classname = keyword + "_info";
-   document.getElementsByClassName(classname)[1].style.display = "table-row";
-   document.getElementsByClassName(classname)[1].style.display = "table-row";
+    //show edit and delete buttons and hide edit options
+	var classname = keyword + "_info";
+	document.getElementsByClassName(classname)[1].style.display = "table-row";
+	document.getElementsByClassName(classname)[1].style.display = "table-row";
+	classname = keyword + "_editbuttons";
+	document.getElementsByClassName(classname)[0].style.display = "none";
+	document.getElementsByClassName(classname)[0].style.display = "none";
+	editing = false;
     
-    classname = keyword + "_editbuttons";
-
-    document.getElementsByClassName(classname)[0].style.display = "none";
-    document.getElementsByClassName(classname)[0].style.display = "none";
-    editing = false;
+    //clear global vars
+	task_id = null;
+	notes_elemid = null;
+	title_elemid = null;
+    totalhours_elemid = null;
+	hours_elemid = null;
+	notes_value = null;
+	title_value = null;
+	hours_value = null;
 }
+
 function edit_task(id, keyword, rowno)
 {
 	notes_elemid = "#" + keyword + "_notes_" + rowno;
-    title_elemid = "#" + keyword + "_title_" + rowno;
-    hours_elemid = "#" + keyword + "_hours_" + rowno;
-    //edit_id = "#" +keyword + "_edit";
+	title_elemid = "#" + keyword + "_title_" + rowno;
+	hours_elemid = "#" + keyword + "_hours_" + rowno;
+    totalhours_elemid = "#" + keyword + '_totalhours';
     
+	//edit_id = "#" +keyword + "_edit";
 	task_id = id;
 	notes_value = $(notes_elemid).text();
 	title_value = $(title_elemid).text();
-    hours_value = $(hours_elemid).text();
-    
-    //prevent 'Notes:' from displaying
-	notes_value  = notes_value .split(": ");
-	notes_value  = notes_value [1];
-    
+	hours_value = $(hours_elemid).text();
+	//prevent 'Notes:' from displaying
+	notes_value = notes_value.split(": ");
+	notes_value = notes_value[1];
 	$(notes_elemid).html('');
 	$('<input></input>').attr(
 	{
-		'value': notes_value ,
+		'value': notes_value,
 		'type': 'text',
 		'id': 'notes_editing'
 	}).appendTo(notes_elemid);
-    console.log(editing);
-    
-    $(hours_elemid).html('');
+	console.log(editing);
+	$(hours_elemid).html('');
 	$('<input></input>').attr(
 	{
-		'value': hours_value ,
-        'size' : '2',
+		'value': hours_value,
+		'size': '2',
 		'type': 'text',
 		'id': 'hours_editing'
 	}).appendTo(hours_elemid);
-    
-    editing = true;
-   	$(title_elemid).html('');
+	editing = true;
+	$(title_elemid).html('');
 	$('<input></input>').attr(
 	{
-		'value': title_value ,
-        'size' : '5',
+		'value': title_value,
+		'size': '5',
 		'type': 'text',
 		'id': 'title_editing'
 	}).appendTo(title_elemid);
 	$('#title_editing').focus();
-    
-   var classname = keyword + "_info";
-   document.getElementsByClassName(classname)[1].style.display = "none";
-   document.getElementsByClassName(classname)[1].style.display = "none";
-    
-    classname = keyword + "_editbuttons";
-
-    document.getElementsByClassName(classname)[0].style.display = "table-row";
-    document.getElementsByClassName(classname)[0].style.display = "table-row";   
-   	
-
+	var classname = keyword + "_info";
+	document.getElementsByClassName(classname)[1].style.display = "none";
+	document.getElementsByClassName(classname)[1].style.display = "none";
+	classname = keyword + "_editbuttons";
+	document.getElementsByClassName(classname)[0].style.display = "table-row";
+	document.getElementsByClassName(classname)[0].style.display = "table-row";
 };
 
 function done_editing(keyword)
 {
-    notes_value = $('#notes_editing').val();
-    title_value = $('#title_editing').val();
-    hours_value = $('#hours_editing').val();
+    var oldhours_value = hours_value;
+	notes_value = $('#notes_editing').val();
+	title_value = $('#title_editing').val();
+	hours_value = $('#hours_editing').val();
+    
+    //update total hours
+    var difference =  oldhours_value - hours_value;
+    
+    var totalhours = $(totalhours_elemid).text() - difference;
+    
+    $(totalhours_elemid).text(totalhours);
+    
 	$(notes_elemid).text("Notes: " + notes_value);
 	$(title_elemid).text(title_value);
-    $(hours_elemid).text(hours_value);
-    
-    //when finished editing send changed to DB
-
-    $.ajax(
+	$(hours_elemid).text(hours_value);
+	//when finished editing send changed to DB
+	$.ajax(
 	{
 		data: {
 			'edit_id': task_id,
 			'edit_notes': notes_value,
-            'edit_title': title_value,
-            'edit_hours' : hours_value                        
+			'edit_title': title_value,
+			'edit_hours': hours_value
 		},
 		url: '../DB/DB_fns.php',
 		method: 'POST'
-   });
-	//clear global vars
-			task_id = null;
-			notes_elemid = null;
-            title_elemid = null;
-            hours_elemid = null;
-            notes_value = null;
-            title_value = null;
-            hours_value = null;
-
-
-    stop_editing(keyword);
- };
-
-
+	});
+	
+	stop_editing(keyword);
+    
+ 
+};
 
 function close_dialogue(delete_id)
 {
